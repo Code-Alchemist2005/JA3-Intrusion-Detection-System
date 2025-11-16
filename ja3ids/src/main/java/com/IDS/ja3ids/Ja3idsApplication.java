68da15c897ec7d08c2d06e6a7fd89d9e338d5f33
Code-Alchemist2005 <shreyagandroli@gmail.com>
2025-11-16 23:04:13 +0530

    Initial project setup with Spring Boot JA3 IDS

---
+++ ja3ids/src/main/java/com/IDS/ja3ids/Ja3idsApplication.java
@@ -0,0 +1,11 @@
+package com.IDS.ja3ids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ja3idsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Ja3idsApplication.class, args);
+    }
+}
---
+++ ja3ids/src/test/java/com/IDS/ja3ids/Ja3idsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IDS.ja3ids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ja3idsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
